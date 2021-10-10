f640708fa75052c6053a2662c7cb94933964ba29
AntwuanDixon <evanwchipman@gmail.com>
2021-10-09 20:10:52 -0700

    move BalancesContainer into MainCard, move MainCard into app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 //import React from 'react';
-import { BalancesContainer } from "./components/BalancesContainer";
-import "./App.css";
-import { LpSpec } from "./utils/balances";
 import { ApiPromise } from "@polkadot/api";
+import "./App.css";
+import MainCard from "./components/MainCard";
 
-const TOKENS = ["KAR", "KSM", "KUSD", "LKSM"];
-const LP_SPECS: Array<LpSpec> = [
-  ["KSM", "LKSM"],
-  ["KUSD", "KSM"],
-  ["KAR", "KSM"],
-];
+
 function App({ api }: { api: ApiPromise }) {
   return (
     <div className="App">
-      <BalancesContainer api={api} tokens={TOKENS} lpSpecs={LP_SPECS} />
+      <MainCard api={api}/>
     </div>
   );
 }
