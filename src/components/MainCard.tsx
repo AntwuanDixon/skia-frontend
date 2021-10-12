41cbea7933b2daf7f04e92c2199cd797509ddaed
AntwuanDixon <evanwchipman@gmail.com>
2021-10-11 21:43:44 -0700

    move address input to card, send results of address query to tab panel as props

--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -1,27 +1,52 @@
 import { Box, Container } from "@mui/material";
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import { BalancesContainer } from "./BalancesContainer";
-import { LpSpec } from "../utils/balances";
+import AddressInput from "./AddressInput";
+import { getBalances } from "../utils/balances";
 import { ApiPromise } from "@polkadot/api";
 import StakeCard from "./StakeCard";
 import TabPanel from "./TabPanel";
+import { isKaruraAddress } from "../utils";
+
+export type BalanceDict = Record<string, bigint>;
+export type BalanceList = Array<{ token: string; balance: bigint }>;
+export type LpSpec = [string, string];
 
 interface Props {
   api: ApiPromise;
 }
 
-const TOKENS = ["KAR", "KSM", "KUSD", "LKSM"];
-const LP_SPECS: Array<LpSpec> = [
+const lpSpecs: Array<LpSpec> = [
   ["KSM", "LKSM"],
   ["KUSD", "KSM"],
   ["KAR", "KSM"],
 ];
 
-
 function MainCard({ api }: Props): ReactElement {
+  const [statusMsg, setStatusMsg] = useState("");
+  const [balances, setBalances] = useState<BalanceDict>({});
+  const [tokens, setTokens] = useState(["KAR", "KSM", "KUSD", "LKSM"]);
+
+  const handleAddressInput = async (address: string) => {
+    setStatusMsg(
+      isKaruraAddress(address) ? "Fetching data..." : "Your address is invalid"
+    );
+    try {
+      const balances = await getBalances(address, { tokens, lpSpecs, api });
+      setBalances(balances);
+      const sortedTokens = Object.keys(balances).sort();
+      setTokens(sortedTokens);
+      return { balances, sortedTokens };
+    } catch (err) {
+      const statusMessage = `Querying failed!)`;
+      setStatusMsg(statusMessage);
+      return null;
+    }
+  };
+
   return (
     <Container maxWidth="lg">
-      <BalancesContainer api={api} tokens={TOKENS} lpSpecs={LP_SPECS} />
+      <AddressInput handler={handleAddressInput} />
       <Box
         sx={{
           width: "100%",
@@ -31,8 +56,8 @@ function MainCard({ api }: Props): ReactElement {
           padding: ".3rem",
           borderRadius: "1rem",
           display: "flex",
-          justifyContent: 'space-between',
-          alignItems: 'flex-start'
+          justifyContent: "space-between",
+          alignItems: "flex-start",
         }}
       >
         <Box
@@ -46,7 +71,7 @@ function MainCard({ api }: Props): ReactElement {
         >
           <StakeCard />
         </Box>
-        
+
         <Box
           sx={{
             borderRadius: "1rem",
@@ -56,7 +81,8 @@ function MainCard({ api }: Props): ReactElement {
             alignItems: "center",
           }}
         >
-          <TabPanel />
+          {tokens[0] !== "" ? (<TabPanel tokens={tokens} balances={balances} api={api} />) : null}
+          
         </Box>
       </Box>
     </Container>
