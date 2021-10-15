4841cb86b3fbd28a9adccb8a9bd8e0ee9a794da9
AntwuanDixon <evanwchipman@gmail.com>
2021-10-15 15:38:49 -0700

    change syntax for RPCs to from kusama to mandala testnet format, add type declarations to functions

--- src/utils/liquidStake.ts
+++ src/utils/liquidStake.ts
@@ -1,32 +1,24 @@
 import { ApiPromise } from "@polkadot/api";
 
-export type BalanceDict = Record<string, bigint>;
-export type BalanceList = Array<{ token: string; balance: bigint }>;
-export type LpSpec = [string, string];
-declare type AnyJson = string | number | boolean | null | undefined | AnyJson[] | {
-    [index: string]: AnyJson;
-};
-
-
 export async function getMintFee(
     api: ApiPromise
   ): Promise<AnyJson> {
     try{
-        const mintFee = api.consts.homaLite.mintFee.toString()
-        const mintFeeHuman = await convertTokenValueToHuman (api, mintFee)
+        const mintFee = api.consts.homaLite.mintFeeHuman.toString()
+        const mintFeeHuman = await convertTokenValueToHuman(api, mintFee)
         return mintFeeHuman.toString()
       } catch (err) {
         console.log(err)
       }
   }
 
-async function convertTokenValueToHuman (api, rawNum) {
+async function convertTokenValueToHuman (api: ApiPromise, rawNum: string): Promise<number> {
   const properties = await api.rpc.system.properties()
   const tokenDecimals = properties.get("tokenDecimals").toHuman()
   return parseInt(rawNum) / (10 ** (parseInt(tokenDecimals[0]) - 1))
 }
 
-async function convertTokenValueToMachine (api, humanNum) {
+async function convertTokenValueToMachine (api: ApiPromise, humanNum: string): Promise<number> {
   const properties = await api.rpc.system.properties()
   const tokenDecimals = properties.get("tokenDecimals").toHuman()
   return parseFloat(humanNum) * (10 ** (parseInt(tokenDecimals[0]) - 1))
@@ -36,13 +28,10 @@ async function convertTokenValueToMachine (api, humanNum) {
 export async function mintLKSM(
     address: string,
     api: ApiPromise,
-    amount: BigInt
+    amount: string
 ): Promise<AnyJson> {
     const rawAmount = await convertTokenValueToMachine(api, amount)
-    console.log(rawAmount)
-    console.log(address)
     const extrinsic = api.tx.homaLite.mint(rawAmount);
     const hash = await extrinsic.signAndSend(address);
-    console.log('hash', hash.toHuman());
     return hash.toHuman()
 }
