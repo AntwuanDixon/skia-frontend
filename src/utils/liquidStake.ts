85f08206d8b19d4fe71fe723df0a08ce66fcd33b
AntwuanDixon <evanwchipman@gmail.com>
2021-10-14 17:17:55 -0700

    convert human token values to machine and vice versa

--- src/utils/liquidStake.ts
+++ src/utils/liquidStake.ts
@@ -1,5 +1,4 @@
 import { ApiPromise } from "@polkadot/api";
-import { Hash } from '@polkadot/types/interfaces'
 
 export type BalanceDict = Record<string, bigint>;
 export type BalanceList = Array<{ token: string; balance: bigint }>;
@@ -9,44 +8,41 @@ declare type AnyJson = string | number | boolean | null | undefined | AnyJson[] 
 };
 
 
-async function getMintFee(
+export async function getMintFee(
     api: ApiPromise
   ): Promise<AnyJson> {
     try{
-        const mintFee = api.consts.homaLite.mintFee.toHuman()
-        console.log(mintFee)
-        return mintFee
+        const mintFee = api.consts.homaLite.mintFee.toString()
+        const mintFeeHuman = await convertTokenValueToHuman (api, mintFee)
+        return mintFeeHuman.toString()
       } catch (err) {
         console.log(err)
       }
   }
 
-async function mintLKSM(
+async function convertTokenValueToHuman (api, rawNum) {
+  const properties = await api.rpc.system.properties()
+  const tokenDecimals = properties.get("tokenDecimals").toHuman()
+  return parseInt(rawNum) / (10 ** (parseInt(tokenDecimals[0]) - 1))
+}
+
+async function convertTokenValueToMachine (api, humanNum) {
+  const properties = await api.rpc.system.properties()
+  const tokenDecimals = properties.get("tokenDecimals").toHuman()
+  return parseFloat(humanNum) * (10 ** (parseInt(tokenDecimals[0]) - 1))
+}
+  
+
+export async function mintLKSM(
     address: string,
     api: ApiPromise,
     amount: BigInt
 ): Promise<AnyJson> {
-    const extrinsic = api.tx.homaLite.mint( amount );
+    const rawAmount = await convertTokenValueToMachine(api, amount)
+    console.log(rawAmount)
+    console.log(address)
+    const extrinsic = api.tx.homaLite.mint(rawAmount);
     const hash = await extrinsic.signAndSend(address);
     console.log('hash', hash.toHuman());
     return hash.toHuman()
 }
-
-  // // Retrieve the chain name
-  // const chain = await api.rpc.system.chain();
-
-  // // Retrieve the latest header
-  // // const lastHeader = await api.rpc.chain.getHeader();
-  // let count = 0;
-  // // Log the information
-  // const unsubHeads = await api.rpc.chain.subscribeNewHeads((lastHeader) => {
-  //   console.log(`${chain}: last block #${lastHeader.number} has hash ${lastHeader.hash}`);
-
-  //   if (++count === 10) {
-  //     unsubHeads();
-  //   }
-  // });
-
-  // const unsub = await api.query.system.account(address, ({ nonce, data: balance }) => {
-  //   console.log(`free balance is ${balance.free} with ${balance.reserved} reserved and a nonce of ${nonce}`);
-  // });
