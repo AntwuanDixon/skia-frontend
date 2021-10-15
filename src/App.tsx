57738d8d962ea041346cc2a6375e02bffc0c28d4
AntwuanDixon <evanwchipman@gmail.com>
2021-10-15 15:33:11 -0700

    refactor user account and api state with context providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-//import React from 'react';
 import { ApiPromise } from "@polkadot/api";
 import "./App.css";
 import MainCard from "./components/MainCard";
+import { useEffect, createContext, useReducer } from "react";
+import { appReducer, apiReducer, defaultAccount } from "./utils/Reducers"
 
+export const AppContext = createContext<AppContextType>(defaultAccount);
+export const ApiContext = createContext<any>("");
 
 function App({ api }: { api: ApiPromise }) {
+  const [state, dispatch] = useReducer(appReducer, []);
+  const [API, dispatchAPI] = useReducer(apiReducer, []);
+  useEffect(() => {
+    dispatchAPI({ type: "SET_API", payload: api });
+  }, [api]);
+
   return (
-    <div className="App">
-      <MainCard api={api}/>
-    </div>
+    <ApiContext.Provider value={[API, dispatchAPI]}>
+      <AppContext.Provider value={{state, dispatch}}>
+        <div className="App">
+          <MainCard />
+        </div>
+      </AppContext.Provider>
+    </ApiContext.Provider>
   );
 }
 
